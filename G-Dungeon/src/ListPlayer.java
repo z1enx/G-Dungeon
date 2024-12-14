dd80eb1ad544673f71cc51f253a2cb3f29c92bb2
Ridho2830 <www.inya2830@gmail.com>
2024-12-14 09:46:29 +0700

    player ready

---
+++ G-Dungeon/src/ListPlayer.java
@@ -0,0 +1,22 @@
+
+public class ListPlayer {
+
+    Player head;
+    Player tail;
+
+    public ListPlayer() {
+        head = null;
+        tail = null;
+    }
+
+    public void addPlayer(int turn, String name, double attack, double defense, double health, double mana, String role) {
+        Player newPlayer = new Player(turn, name, attack, defense, health, mana, role);
+        if (head == null) {
+            head = newPlayer;
+            tail = newPlayer;
+        } else {
+            tail.next = newPlayer;
+            tail = newPlayer;
+        }
+    }
+}
---
+++ G-Dungeon/src/Player.java
@@ -0,0 +1,23 @@
+
+public class Player {
+
+    int turn;
+    String name;
+    double attack;
+    double defense;
+    double health;
+    double mana;
+    String role;
+    Player next;
+
+    public Player(int turn,String name, double attack, double defense, double health, double mana, String role) {
+        this.turn = turn;
+        this.name = name;
+        this.attack = attack;
+        this.defense = defense;
+        this.health = health;
+        this.mana = mana;
+        this.role = role;
+        this.next = null;
+    }
+}
